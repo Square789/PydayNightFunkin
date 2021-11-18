22550a114e6b63faac91bd07ebdafdfee00b8034
Square789 <46634729+Square789@users.noreply.github.com>
2021-11-18 11:14:13 +0100

    Condense PNFSpriteContainer screen centering

--- pyday_night_funkin/graphics/pnf_sprite_container.py
+++ pyday_night_funkin/graphics/pnf_sprite_container.py
@@ -98,58 +98,30 @@ class PNFSpriteContainer(PNFSprite):
 		for sprite in self._sprites:
 			func(sprite, val)
 
-	def screen_center(self, game_dims: "Vec2", x: bool = True, y: bool = True) -> None:
-		"""
-		Centers all sprites in this SpriteContainer along the given
-		axes in respect to the supplied game dimensions.
-		"""
-		if not self._sprites:
-			return
-
-		# NOTE: this may loop over all sprites 6 times when 1 time
-		# would be possible too. Don't see any need for extreme over-
-		# optimization in that regard for now
-		if x:
-			max_x = self.max_x
-			min_x = self.min_x
-			offset = (game_dims[0] - abs(max_x - min_x)) // 2
-			for sprite in self._sprites:
-				sprite.x = (sprite.x - min_x) + offset
-		if y:
-			max_y = self.max_y
-			min_y = self.min_y
-			offset = (game_dims[1] - abs(max_y - min_y)) // 2
-			for sprite in self._sprites:
-				sprite.y = (sprite.y - min_y) + offset
-
-	@property
-	def min_x(self) -> t.Optional["Numeric"]:
-		if not self._sprites:
-			return None
-		return min(sprite.x for sprite in self._sprites)
-
-	@property
-	def max_x(self) -> t.Optional["Numeric"]:
-		if not self._sprites:
-			return None
-		return max(sprite.x + sprite.signed_width for sprite in self._sprites)
-
-	@property
-	def min_y(self) -> t.Optional["Numeric"]:
-		if not self._sprites:
-			return None
-		return min(sprite.y for sprite in self._sprites)
-
-	@property
-	def max_y(self) -> t.Optional["Numeric"]:
-		if not self._sprites:
-			return None
-		return max(sprite.y + sprite.signed_height for sprite in self._sprites)
-
 	def __iter__(self) -> t.Iterator[PNFSprite]:
 		yield from self._sprites
 
 	# === PNFSprite property overrides === #
+
+	@property
+	def signed_width(self) -> float:
+		if not self._sprites:
+			return 0
+		return (
+			max(sprite.x + sprite.signed_width for sprite in self._sprites) -
+			min(sprite.x for sprite in self._sprites)
+		)
+
+	@property
+	def signed_height(self) -> float:
+		if not self._sprites:
+			return 0
+		return (
+			max(sprite.y + sprite.signed_height for sprite in self._sprites) -
+			min(sprite.y for sprite in self._sprites)
+		)
+
+	# === PNFSprite property setter overrides === #
 
 	@staticmethod
 	def _transform_x(sprite: PNFSprite, x: "Numeric") -> None:
