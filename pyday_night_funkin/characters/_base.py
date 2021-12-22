07b16f81cd7b5ed36ee77fb18878c9ef7dc00283
Square789 <46634729+Square789@users.noreply.github.com>
2021-12-22 21:10:18 +0100

    Hold timeout is now realized via a staticmethod.

--- pyday_night_funkin/characters/_base.py
+++ pyday_night_funkin/characters/_base.py
@@ -6,17 +6,16 @@ from pyday_night_funkin.core.pnf_sprite import PNFSprite
 
 if t.TYPE_CHECKING:
 	from pyday_night_funkin.scenes import MusicBeatScene
+	from pyday_night_funkin.types import Numeric
 
 
 class Character(PNFSprite):
-
-	hold_timeout = 4.0
 
 	def __init__(self, scene: "MusicBeatScene", *args, **kwargs) -> None:
 		super().__init__(*args, **kwargs)
 
 		self.scene = scene
-
+		self._hold_timeout = self.get_hold_timeout()
 		self.hold_timer = 0.0
 		self.dont_idle = False
 
@@ -26,8 +25,16 @@ class Character(PNFSprite):
 			self.hold_timer += dt
 
 		if (
-			self.hold_timer >= self.hold_timeout * self.scene.conductor.step_duration * 0.001 and
+			self.hold_timer >= self._hold_timeout * self.scene.conductor.step_duration * 0.001 and
 			not self.dont_idle
 		):
 			self.hold_timer = 0.0
 			self.animation.play("idle_bop")
+
+	@staticmethod
+	def get_hold_timeout() -> "Numeric":
+		"""
+		Returns how many steps the character should remain in their
+		sing animation for after singing a note. Default is 4.
+		"""
+		return 4
--- pyday_night_funkin/characters/daddy_dearest.py
+++ pyday_night_funkin/characters/daddy_dearest.py
@@ -5,11 +5,11 @@ from pyday_night_funkin.asset_system import ASSETS, load_asset
 from pyday_night_funkin.characters._base import Character
 from pyday_night_funkin.enums import ANIMATION_TAG
 
+if t.TYPE_CHECKING:
+	from pyday_night_funkin.types import Numeric
+
 
 class DaddyDearest(Character):
-
-	# Idk why but if the original game says so
-	hold_timeout = 6.1
 
 	def __init__(self, *args, **kwargs) -> None:
 		super().__init__(*args, **kwargs)
@@ -41,3 +41,8 @@ class DaddyDearest(Character):
 			"sing_note_right", anims["Dad Sing Note RIGHT"], 24, False, (0, 27),
 			(ANIMATION_TAG.SING, )
 		)
+
+	# Idk why but if the original game says so
+	@staticmethod
+	def get_hold_timeout() -> "Numeric":
+		return 6.1
